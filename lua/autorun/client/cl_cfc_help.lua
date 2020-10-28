9b11eb28907d46285461dc438a1489e6b1585e86
wrefgtzweve <jasperdk@planet.nl>
2020-10-29 00:51:21 +0100

    Removed unnecesscary comments and made sure to apply guidelines.

--- lua/autorun/client/cl_cfc_help.lua
+++ lua/autorun/client/cl_cfc_help.lua
@@ -1,8 +1,8 @@
 local function openHelp()
 	local window = vgui.Create( "DFrame" )
 
-	if ScrW() > 640 then -- Make it larger if we can.
-		window:SetSize( ScrW()*0.9, ScrH()*0.9 )
+	if ScrW() > 640 then
+		window:SetSize( ScrW() * 0.9, ScrH() * 0.9 )
 	else
 		window:SetSize( 640, 480 )
 	end
@@ -11,8 +11,8 @@ local function openHelp()
 	window:SetTitle( "CFC Help" )
 	window:MakePopup()
 	
-	window.Paint = function( self, w, h ) -- 'function Frame:Paint( w, h )' works too
-	draw.RoundedBox( 0, 0, 0, w, h, Color( 36, 41, 67, 255 ) ) -- Draw a red box instead of the frame
+	window.Paint = function( self, w, h )
+	draw.RoundedBox( 0, 0, 0, w, h, Color( 36, 41, 67, 255 ) )
 	end
 
 	local html = vgui.Create( "DHTML", window )
@@ -22,14 +22,14 @@ local function openHelp()
 end
 
 local function hideScrollbar ()
-	--WIP
+	-- WIP
 end
 
-concommand.Add( "cfc_help", openHelp)
+concommand.Add( "cfc_help", openHelp )
 
 hook.Add( "OnPlayerChat", "CFC_Help_OpenHelpCommand", function( ply, msg )
 	
-	if string.sub(msg,1,5) ~= "!help" then return end
+	if string.sub( msg, 1, 5 ) ~= "!help" then return end
 	
     if ply == LocalPlayer() then
         openHelp()
