454cf1455c02196f1ff96b910e4e0f3418dad7b0
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2020-10-29 22:34:18 +0100

    Add rounded corners and fix tabs

--- lua/autorun/client/cl_cfc_help.lua
+++ lua/autorun/client/cl_cfc_help.lua
@@ -1,39 +1,39 @@
 local function openHelp()
-	local window = vgui.Create( "DFrame" )
+    local window = vgui.Create( "DFrame" )
 
-	if ScrW() > 640 then
-		window:SetSize( ScrW() * 0.9, ScrH() * 0.9 )
-	else
-		window:SetSize( 640, 480 )
-	end
-	
-	window:Center()
-	window:SetTitle( "CFC Help" )
-	window:MakePopup()
-	
-	window.Paint = function( self, w, h )
-	draw.RoundedBox( 0, 0, 0, w, h, Color( 36, 41, 67, 255 ) )
-	end
+    if ScrW() > 640 then
+        window:SetSize( ScrW() * 0.9, ScrH() * 0.9 )
+    else
+        window:SetSize( 640, 480 )
+    end
 
-	local html = vgui.Create( "DHTML", window )
-	
-	html:Dock( FILL )
-	html:OpenURL("https://cfcservers.org/learn/")
+    window:Center()
+    window:SetTitle( "CFC Help" )
+    window:MakePopup()
+
+    window.Paint = function( self, w, h )
+    draw.RoundedBox( 8, 0, 0, w, h, Color( 36, 41, 67, 255 ) )
+    end
+
+    local html = vgui.Create( "DHTML", window )
+
+    html:Dock( FILL )
+    html:OpenURL("https://cfcservers.org/learn/")
 end
 
 local function hideScrollbar ()
-	-- WIP
+    -- WIP
 end
 
 concommand.Add( "cfc_help", openHelp )
 
 hook.Add( "OnPlayerChat", "CFC_Help_OpenHelpCommand", function( ply, msg )
-	
-	if string.sub( msg, 1, 5 ) ~= "!help" then return end
-	
+
+    if string.sub( msg, 1, 5 ) ~= "!help" then return end
+
     if ply == LocalPlayer() then
         openHelp()
-	end
-	
-	return true
+    end
+
+    return true
 end )
