5fd1915e135cf727fd32cf16f7ac923a30eb0dd7
Brandon Sturgeon <brandon@brandonsturgeon.com>
2021-02-16 11:46:22 -0800

    Refactor and additions

--- lua/autorun/client/cl_cfc_help.lua
+++ lua/autorun/client/cl_cfc_help.lua
@@ -1,6 +1,47 @@
-local uiColor = Color( 36, 41, 67, 255 )
+CFCHelp = {
+    colors = {
+        ui = Color( 36, 41, 67, 255 ),
+        lightBlue = Color( 83, 227, 251, 255 ),
+        lightGreen = Color( 83, 251, 191 ),
+        white = Color( 255, 255, 255, 255 ),
+        grey = Color( 175, 175, 175, 255 )
+    },
 
-local function openHelp()
+    commandMap = {
+        ["!help"] = {
+            helpType = "html",
+            url = "https://cfcservers.org/learn/player",
+            title = "CFC Help"
+        },
+
+        ["!rules"] = {
+            helpType = "html",
+            url = "https://cfcservers.org/cfc3/rules",
+            title = "CFC Rules"
+        },
+
+        ["!motd"] = {
+            helpType = "html",
+            url = "https://cfcservers.org/cfc3/shortrules",
+            title = "CFC Rules"
+        },
+
+        ["!discord"] = {
+            helpType = "function",
+            func = function()
+                chat.AddText(
+                    CFCHelp.colors.lightGreen,
+                    "Visit the following URL in your browser: ",
+                    CFCHelp.colors.lightBlue,
+                    "https://cfcservers.org/discord "
+                )
+            end
+        }
+    }
+}
+
+function CFCHelp:openUrl( url, title )
+    title = title or ""
     local window = vgui.Create( "DFrame" )
 
     if ScrW() > 640 then
@@ -10,31 +51,41 @@ local function openHelp()
     end
 
     window:Center()
-    window:SetTitle( "CFC Help" )
+    window:SetTitle( title )
     window:MakePopup()
 
     window.Paint = function( self, w, h )
-        draw.RoundedBox( 8, 0, 0, w, h, uiColor )
+        draw.RoundedBox( 8, 0, 0, w, h, self.colors.ui )
     end
 
     local html = vgui.Create( "DHTML", window )
 
     html:Dock( FILL )
-    html:OpenURL( "https://cfcservers.org/learn/player/" )
+    html:OpenURL( url )
 end
 
 local function hideScrollbar()
     -- WIP
 end
 
-concommand.Add( "cfc_help", openHelp )
+hook.Add( "OnPlayerChat", "CFC_Help_CommandMatcher", function( ply, msg )
+    local commandInfo = CFCHelp.commandMap[msg]
 
-hook.Add( "OnPlayerChat", "CFC_Help_OpenHelpCommand", function( ply, msg )
-    if not string.StartWith( msg, "!help" ) then return end
+    if not commandInfo then return end
 
     if ply == LocalPlayer() then
-        openHelp()
+        local helpType = commandInfo.helpType
+
+        if helpType == "html" then
+            CFCHelp:openUrl( commandInfo.url, commandInfo.title )
+        end
+
+        if helpType == "func" then
+            commandInfo.func()
+        end
     end
 
-    return true
+    if commandInfo.silent then
+        return true
+    end
 end )
