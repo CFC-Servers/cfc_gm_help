36962dd4a7c555484a3a37899a647293b3e36afb
Brandon Sturgeon <brandon@brandonsturgeon.com>
2021-02-23 12:23:49 -0800

    Dedent

--- lua/autorun/client/cfc_help_init.lua
+++ lua/autorun/client/cfc_help_init.lua
@@ -94,23 +94,20 @@ CFCHelp = {
 
 hook.Add( "OnPlayerChat", "CFC_Help_CommandMatcher", function( ply, msg )
     local commandInfo = CFCHelp.commandMap[msg]
-
     if not commandInfo then return end
 
-    if ply == LocalPlayer() then
-        local helpType = commandInfo.helpType
-
-        if helpType == "html" then
-            CFCHelp:openUrl( commandInfo.url, commandInfo.title )
-        end
-
-        if helpType == "function" then
-            commandInfo.func()
-        end
+    if ply ~= LocalPlayer() then
+        return commandInfo.silent or nil
     end
 
-    if commandInfo.silent then
-        return true
+    local helpType = commandInfo.helpType
+
+    if helpType == "html" then
+        CFCHelp:openUrl( commandInfo.url, commandInfo.title )
+    end
+
+    if helpType == "function" then
+        commandInfo.func()
     end
 end )
 
