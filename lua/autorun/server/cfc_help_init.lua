aee303e9aa3a282c2ba718a05603b6c1b167d8bb
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-11-01 21:41:46 -0700

    Only create serverside

--- lua/autorun/server/cfc_help_init.lua
+++ lua/autorun/server/cfc_help_init.lua
@@ -1,3 +1,4 @@
+CreateConVar( "cfc_realm", "unknown", FCVAR_Replicated + FCVAR_ARCHIVE, "The Realm Name" )
 AddCSLuaFile( "autorun/cfc_help_init.lua" )
 AddCSLuaFile( "autorun/client/cfc_help_init.lua" )
 AddCSLuaFile( "cfc_help/client/cfc_html_panel.lua" )
