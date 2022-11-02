2aed5fe055af3b302af813572ad1859b9bda8a28
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-11-01 21:53:28 -0700

    idk

--- lua/autorun/server/cfc_help_init.lua
+++ lua/autorun/server/cfc_help_init.lua
@@ -1,4 +1,3 @@
-CreateConVar( "cfc_realm", "unknown", FCVAR_Replicated + FCVAR_ARCHIVE, "The Realm Name" )
 AddCSLuaFile( "autorun/cfc_help_init.lua" )
 AddCSLuaFile( "autorun/client/cfc_help_init.lua" )
 AddCSLuaFile( "cfc_help/client/cfc_html_panel.lua" )
